5638abf764b0993edb1d718865605535e20a86b2
Brahiam Musse <bamusse@misena.edu.co>
2018-08-28 16:39:23 -0500

    1

---
+++ app/src/main/java/com/example/worldskills/laera/HOLA.java
@@ -0,0 +1,15 @@
+package com.example.worldskills.laera;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HOLA extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hol);
+
+        // :v
+    }
+}
